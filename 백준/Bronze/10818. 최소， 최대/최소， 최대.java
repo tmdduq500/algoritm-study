8c177fa7a78539bd40bdd89ac3444aa65fa959d2
tmdduq500 <128893813+tmdduq500@users.noreply.github.com>
2023-12-20 17:52:28 +0900

    [Bronze III] Title: 최소, 최대, Time: 560 ms, Memory: 92264 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] list = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            list[i] = Integer.parseInt(st.nextToken());
+        }
+
+        System.out.print(Arrays.stream(list).min().getAsInt() + " ");
+        System.out.println(Arrays.stream(list).max().getAsInt());
+    }
+}
