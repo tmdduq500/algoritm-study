4a2d11953d92bcf2a73b2539667fd8bce858b5bc
tmdduq500 <128893813+tmdduq500@users.noreply.github.com>
2024-09-02 16:53:09 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 21.65 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -3,12 +3,6 @@ class Solution {
         String answer = "";
 
         for (int i = 1; i < food.length; i++) {
-            if(food[i] % 2 != 0) {
-                food[i] = food[i] - 1;
-            }
-        }
-
-        for (int i = 1; i < food.length; i++) {
             for (int j = 0; j < food[i] / 2; j++) {
                 answer += i;
             }
