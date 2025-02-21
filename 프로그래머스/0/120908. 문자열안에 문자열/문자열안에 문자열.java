1d1cf1c63a0f6d22ebb05691aae7a6d609238dbd
tmdduq500 <128893813+tmdduq500@users.noreply.github.com>
2025-02-21 22:19:34 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.03 ms, Memory: 83.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(String str1, String str2) {
+        int answer = 0;
+        
+        if (str1.indexOf(str2) != -1) {
+            answer = 1;
+        } else {
+            answer = 2;
+        }
+
+        
+        return answer;
+    }
+}
