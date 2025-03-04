0fa34b541e385be3c306ba9d32d45846167a4053
tmdduq500 <128893813+tmdduq500@users.noreply.github.com>
2025-03-04 13:03:24 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -1,29 +1,13 @@
 import java.util.*;
 class Solution {
     public int solution(int hp) {
-        int answer = 0;
-        int[] ants = new int[3];
-        Map<String, Integer> map = new HashMap<>();
-        map.put("general", 5);
-        map.put("soldier", 3);
-        map.put("worker", 1);
+        int answer = hp / 5;
+        hp %= 5;
 
-        int generalNums = 0;
-        int soldierNums = 0;
-        int workerNums = 0;
-        int remainHp = 0;
+        answer += hp / 3;
+        hp %= 3;
 
-        generalNums = hp / map.get("general");
-        if(hp % map.get("general") != 0){
-            remainHp = hp % map.get("general");
-            soldierNums = remainHp / map.get("soldier");
-            if(remainHp % map.get("soldier") != 0){
-                remainHp = remainHp % map.get("soldier");
-                workerNums = remainHp / map.get("worker");
-            }
-        }
-
-        answer = generalNums + soldierNums + workerNums;
+        answer += hp / 1;
         return answer;
     }
 }
