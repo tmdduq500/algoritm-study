a34aea4a676c17457eeb1341e867431b172e6ddd
tmdduq500 <128893813+tmdduq500@users.noreply.github.com>
2025-10-23 11:19:23 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.17 ms, Memory: 93.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+
+        String[] split = s.split(" ");
+        for (int i = 0; i < split.length; i++) {
+            if (split[i].equals("Z")) {
+                answer -= Integer.parseInt(split[i - 1]);
+            } else {
+                answer += Integer.parseInt(split[i]);
+            }
+        }
+
+        // System.out.println(answer);
+        return answer;
+    }
+}
