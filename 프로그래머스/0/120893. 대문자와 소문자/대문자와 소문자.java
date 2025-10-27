10d58df7bd8a789576641b39e2c5101b3eac1925
tmdduq500 <128893813+tmdduq500@users.noreply.github.com>
2025-10-27 10:50:56 +0900

    [level 0] Title: 대문자와 소문자, Time: 3.94 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String my_string) {
+        
+    String answer = "";
+        
+    for (int i = 0; i < my_string.length(); i++) {
+        if ((int) my_string.charAt(i) >= 97 && my_string.charAt(i) <= 122) {
+            answer += String.valueOf(my_string.charAt(i)).toUpperCase();
+        } else {
+            answer += String.valueOf(my_string.charAt(i)).toLowerCase();
+        }
+    }
+
+    return answer;
+    }
+}
