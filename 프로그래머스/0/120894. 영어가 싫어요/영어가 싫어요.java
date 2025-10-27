9be701432baeff7715f18766413950a9a7dab515
tmdduq500 <128893813+tmdduq500@users.noreply.github.com>
2025-10-27 11:29:27 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.63 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -2,22 +2,12 @@ class Solution {
     public long solution(String numbers) {
         long answer = 0;
         
-         String answerStr = "";
         String[] numberStrArr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-        int[] numberArr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-
-        String temp = "";
-        for (int i = 0; i < numbers.length(); i++) {
-            temp += numbers.charAt(i);
-            for (int j = 0; j < numberStrArr.length; j++) {
-                if(temp.equals(numberStrArr[j])){
-                    answerStr += numberArr[j];
-                    temp = "";
-                }
-            }
+        for (int i = 0; i < numberStrArr.length; i++) {
+            numbers = numbers.replaceAll(numberStrArr[i], String.valueOf(i));
         }
 
-        answer = Long.parseLong(answerStr);
+        answer = Long.parseLong(numbers);
         return answer;
     }
 }
