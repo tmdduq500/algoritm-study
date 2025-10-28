80d43b589ab497f4f0b619afd4e431c32c90c57f
tmdduq500 <128893813+tmdduq500@users.noreply.github.com>
2025-10-28 09:52:58 +0900

    [level 0] Title: 편지, Time: 0.03 ms, Memory: 96.2 MB -BaekjoonHub

--- 프로그래머스/0/120898. 편지/편지.java
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -1,8 +1,6 @@
 class Solution {
     public int solution(String message) {
         int answer = 0;
-        String[] msgArr = message.split("");
-        answer = 2 * msgArr.length;
-        return answer;
+        return answer = message.length() * 2;
     }
 }
