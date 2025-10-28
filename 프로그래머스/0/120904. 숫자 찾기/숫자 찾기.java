eff674a75f478569fadfa4b62623609573a72e44
tmdduq500 <128893813+tmdduq500@users.noreply.github.com>
2025-10-28 11:32:49 +0900

    [level 0] Title: 숫자 찾기, Time: 0.04 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,15 +1,10 @@
 class Solution {
     public int solution(int num, int k) {
         int answer = 0;
-        String[] split = String.valueOf(num).split("");
-        for (int i = 0; i < split.length; i++) {
-            if (split[i].equals(String.valueOf(k)) && answer == 0) {
-                answer = i + 1;
-            }
-        }
-        if (answer == 0) {
-            answer = -1;
-        }
+        String numStr = String.valueOf(num);
+        String kStr = String.valueOf(k);
+
+        answer = numStr.indexOf(kStr) + 1 == 0 ? -1 : numStr.indexOf(kStr) + 1; 
         return answer;
     }
 }
