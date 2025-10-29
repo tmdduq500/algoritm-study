db2f5ed08dcc66cd6bee6794f75d9f355f2edfdc
tmdduq500 <128893813+tmdduq500@users.noreply.github.com>
2025-10-29 10:32:47 +0900

    [level 0] Title: 세균 증식, Time: 0.03 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n, int t) {
+        int answer = 0;
+        while (t > 0) {
+            n *= 2;
+            t--;
+        }
+
+        answer = n;
+        return answer;
+    }
+}
